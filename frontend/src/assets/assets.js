7136565e7ba551318f335ecb9ad2cd6fc436ac97
jagdish gupta <varshneyjacky@gmail.com>
2025-04-13 15:23:31 +0530

    edit

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -56,7 +56,9 @@ import p_img52 from './p_img52.png'
 
 
 import logo from './logo.png'
+import logo3 from './logo3.png'
 import hero_img from './hero_img.png'
+import hero_img2 from './hero_img2.jpeg'
 import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
 import dropdown_icon from './dropdown_icon.png'
@@ -76,7 +78,9 @@ import cross_icon from './cross_icon.png'
 
 export const assets = {
     logo,
+    logo3,
     hero_img,
+    hero_img2,
     cart_icon,
     dropdown_icon,
     exchange_icon,
