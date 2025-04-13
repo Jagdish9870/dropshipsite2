cff20522728686dbde3e00425062aeb44c97438e
jagdish gupta <varshneyjacky@gmail.com>
2025-04-13 13:43:54 +0530

    ad

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 
     mongoose.connection.on('connected',() => {
-        console.log("DB Connected ", `${process.env.MONGODB_URI}/willsThrills`);
+        console.log("DB Connected ", `${process.env.MONGODB_URI}`);
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/willsThrills`)
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
 
 }
 
