ceab78e7aef7c82923cd7b9021a9014be8ce2609
jagdish gupta <varshneyjacky@gmail.com>
2025-04-13 13:24:57 +0530

    correction

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 
     mongoose.connection.on('connected',() => {
-        console.log("DB Connected ", `${process.env.MONGODB_URI}/ecommerce`);
+        console.log("DB Connected ", `${process.env.MONGODB_URI}`);
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`)
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
 
 }
 
