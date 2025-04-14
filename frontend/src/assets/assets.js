29f1da3ce3046932dd2b4f5b86676e1f48fef543
jagdish gupta <varshneyjacky@gmail.com>
2025-04-14 09:48:54 +0530

    image

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -54,7 +54,7 @@ import p_img50 from './p_img50.png'
 import p_img51 from './p_img51.png'
 import p_img52 from './p_img52.png'
 
-import hero from './hero.png'
+import hero from './hero.jpeg'
 import logo from './logo.png'
 import hero_img from './hero_img.png'
 import cart_icon from './cart_icon.png'
