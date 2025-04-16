8583ec15df7dd4836a7f64333e28fe24ddba20bf
jagdish gupta <varshneyjacky@gmail.com>
2025-04-16 14:14:36 +0530

    alignment of foooter

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,12 +8,13 @@ const Footer = () => {
     <div>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 
-        <div>
-            <img src={assets.logo2} className='mb-5 w-32' alt="" />
-            <p className='w-full md:w-2/3 text-gray-600'>
-            Wills Thrills was born out of a passion for innovation and a vision to redefine the way people explore and experience electronic products online
-            </p>
-        </div>
+      <div className="flex items-start gap-4 mb-5">
+  <img src={assets.logo2} className="w-32" alt="logo" />
+  <p className="text-gray-600 max-w-xl">
+    Wills Thrills was born out of a passion for innovation and a vision to redefine the way people explore and experience electronic products online
+  </p>
+</div>
+
 
         <div>
             <p className='text-xl font-medium mb-5'>Wills Thrills</p>
