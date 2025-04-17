d5eca465e9d718d52f2bfba0049ff5d59546c751
jagdish gupta <varshneyjacky@gmail.com>
2025-04-17 15:39:33 +0530

    showing discount

--- frontend/src/components/CartTotal.jsx
+++ frontend/src/components/CartTotal.jsx
@@ -35,6 +35,59 @@
 // export default CartTotal
 
 
+// import React, { useContext } from 'react';
+// import { ShopContext } from '../context/ShopContext';
+// import Title from './Title';
+
+// const CartTotal = ({ discount = 0 }) => {
+//   const { currency, delivery_fee, getCartAmount } = useContext(ShopContext);
+
+//   const subtotal = getCartAmount();
+//   let discountAmount = 0;
+
+//   if (typeof discount === 'number') {
+//     discountAmount = discount < 1 ? subtotal * discount : discount;
+//   }
+
+//   const total = subtotal === 0 ? 0 : subtotal + delivery_fee - discountAmount;
+
+//   return (
+//     <div className='w-full'>
+//       <div className='text-2xl'>
+//         <Title text1={'CART'} text2={'TOTALS'} />
+//       </div>
+
+//       <div className='flex flex-col gap-2 mt-2 text-sm'>
+//         <div className='flex justify-between'>
+//           <p>Subtotal</p>
+//           <p>{currency} {subtotal.toFixed(2)}</p>
+//         </div>
+//         <hr />
+//         <div className='flex justify-between'>
+//           <p>Shipping Fee</p>
+//           <p>{currency} {delivery_fee.toFixed(2)}</p>
+//         </div>
+//         {discountAmount > 0 && (
+//           <>
+//             <hr />
+//             <div className='flex justify-between text-green-600'>
+//               <p>Discount</p>
+//               <p>- {currency} {discountAmount.toFixed(2)}</p>
+//             </div>
+//           </>
+//         )}
+//         <hr />
+//         <div className='flex justify-between'>
+//           <b>Total</b>
+//           <b>{currency} {total.toFixed(2)}</b>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CartTotal;
+
 import React, { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
@@ -49,7 +102,8 @@ const CartTotal = ({ discount = 0 }) => {
     discountAmount = discount < 1 ? subtotal * discount : discount;
   }
 
-  const total = subtotal === 0 ? 0 : subtotal + delivery_fee - discountAmount;
+  const discountedSubtotal = subtotal - discountAmount;
+  const total = subtotal === 0 ? 0 : discountedSubtotal + delivery_fee;
 
   return (
     <div className='w-full'>
@@ -60,13 +114,23 @@ const CartTotal = ({ discount = 0 }) => {
       <div className='flex flex-col gap-2 mt-2 text-sm'>
         <div className='flex justify-between'>
           <p>Subtotal</p>
-          <p>{currency} {subtotal.toFixed(2)}</p>
+          {discountAmount > 0 ? (
+            <p>
+              <span className="line-through text-gray-400 mr-2">{currency} {subtotal.toFixed(2)}</span>
+              <span className="text-green-600">{currency} {discountedSubtotal.toFixed(2)}</span>
+            </p>
+          ) : (
+            <p>{currency} {subtotal.toFixed(2)}</p>
+          )}
         </div>
+
         <hr />
+
         <div className='flex justify-between'>
           <p>Shipping Fee</p>
           <p>{currency} {delivery_fee.toFixed(2)}</p>
         </div>
+
         {discountAmount > 0 && (
           <>
             <hr />
@@ -76,7 +140,9 @@ const CartTotal = ({ discount = 0 }) => {
             </div>
           </>
         )}
+
         <hr />
+
         <div className='flex justify-between'>
           <b>Total</b>
           <b>{currency} {total.toFixed(2)}</b>
